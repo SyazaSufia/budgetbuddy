1ab3c1fa24769bd3da47e4304a8f23482383a56e
syaza <>
2025-04-08 22:38:08 +0800

    remove

--- src/BudgetPage/BudgetDetails/CategoryTitle.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./BudgetDetails.module.css";
-
-function CategoryTitle({ title }) {
-  return <h1 className={styles.div7}>{title}</h1>;
-}
-
-export default CategoryTitle;
